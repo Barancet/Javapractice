692766f141b68b0467d1f47b22b22efc614c1b92
barancet <barcet95@gmail.com>
2020-01-28 03:05:47 -0500

    string buffer change

--- src/hackerrankq/StringPrac.java
+++ src/hackerrankq/StringPrac.java
@@ -4,19 +4,18 @@ public class StringPrac {
 
     public static void main(String[] args){
 
-        String str = "cool bro that is sick";
+        String str = "cool nice wow";
 
         System.out.println(reverseString(str));
 
     }
 
     public static String reverseString(String oldString){
-        String reversedString = "";
-
+        StringBuilder sb = new StringBuilder();
         for(int i = oldString.length()-1;i>=0 ; i--){
-           reversedString += oldString.charAt(i);
+           sb = sb.append(oldString.charAt(i));
         }
 
-        return reversedString;
+        return sb.toString();
     }
 }
